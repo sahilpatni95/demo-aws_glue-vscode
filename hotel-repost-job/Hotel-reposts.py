cb1850b70630405a812c59bfd71014c1b493a7c3
Sahil Patni <sahilpatni95@outlook.com>
2023-02-21 23:40:58 +0530

    update

--- hotel-repost-job/Hotel-reposts.py
+++ hotel-repost-job/Hotel-reposts.py
@@ -1,46 +1,26 @@
-# import sys
-# from awsglue.transforms import *
-# from awsglue.utils import getResolvedOptions
-# from pyspark.context import SparkContext
-# from awsglue.context import GlueContext
-# from awsglue.job import Job
-
-# ## @params: [JOB_NAME]
-# args = getResolvedOptions(sys.argv, ['JOB_NAME'])
-
-# sc = SparkContext()
-# glueContext = GlueContext(sc)
-# spark = glueContext.spark_session
-# job = Job(glueContext)
-# job.init(args['JOB_NAME'], args)
-# job.commit()
-
 #########################################
 ### IMPORT LIBRARIES AND SET VARIABLES
 #########################################
-
-#Import python modules
-from datetime import datetime
-
-#Import pyspark modules
+import sys
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
 from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.job import Job
+from awsglue.dynamicframe import DynamicFrame
+from datetime import datetime
 import pyspark.sql.functions as f
 
-#Import glue modules
-from awsglue.utils import getResolvedOptions
-from awsglue.context import GlueContext
-from awsglue.dynamicframe import DynamicFrame
-from awsglue.job import Job
-
 #Initialize contexts and session
-spark_context = SparkContext.getOrCreate()
-glue_context = GlueContext(spark_context)
-session = glue_context.spark_session
+sc = SparkContext.getOrCreate()
+gc = GlueContext(sc)
+session = gc.spark_session
 
 #Parameters
 glue_db = "hotel-22022023"
 glue_tbl = "raw"
 s3_write_path = "s3://hotaldata-22022023/processes"
+s3_write_path_Cleaned = "s3://hotaldata-22022023/Cleaned"
 
 #########################################
 ### EXTRACT (READ DATA)
@@ -51,12 +31,13 @@ dt_start = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 print("Start time:", dt_start)
 
 #Read movie data to Glue dynamic frame
-dynamic_frame_read = glue_context.create_dynamic_frame.from_catalog(
-    database=glue_db, table_name=glue_tbl)
+dynamic_frame_read = gc\
+    .create_dynamic_frame\
+    .from_catalog(database=glue_db, table_name=glue_tbl)
 
 #Convert dynamic frame to data frame to use standard pyspark functions
 data_frame = dynamic_frame_read.toDF()
-
+'''
 #########################################
 ### TRANSFORM (MODIFY DATA)
 #########################################
@@ -78,7 +59,7 @@ data_frame = dynamic_frame_read.toDF()
 # #Note: Show function is an action. Actions force the execution of the data frame plan.
 # #With big data the slowdown would be significant without cacching.
 # data_frame_aggregated.show(10)
-
+'''
 #########################################
 ### LOAD (WRITE DATA)
 #########################################
@@ -88,10 +69,10 @@ data_frame_aggregated = data_frame.repartition(1)
 
 #Convert back to dynamic frame
 dynamic_frame_write = DynamicFrame.fromDF(
-    data_frame_aggregated, glue_context, "dynamic_frame_write")
+    data_frame_aggregated, gc, "dynamic_frame_write")
 
 #Write data back to S3
-glue_context.write_dynamic_frame.from_options(
+gc.write_dynamic_frame.from_options(
     frame=dynamic_frame_write,
     connection_type="s3",
     connection_options={
@@ -100,6 +81,17 @@ glue_context.write_dynamic_frame.from_options(
         #"partitionKeys": ["decade"]
     },
     format="parquet"
+)
+
+gc.write_dynamic_frame.from_options(
+    frame=dynamic_frame_write,
+    connection_type="s3",
+    connection_options={
+        "path": s3_write_path_Cleaned,
+        #Here you could create S3 prefixes according to a values in specified columns
+        #"partitionKeys": ["decade"]
+    },
+    format="csv"
 )
 
 #Log end time
